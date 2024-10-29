087b6457a37b1213e7266fbfe716172e03d98a2a
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-10-29 10:58:03 +0100

    update

--- teamrush/src/App.tsx
+++ teamrush/src/App.tsx
@@ -1,43 +1,37 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import DevCard from './components/DevCard';
-import DevNavBar from './components/DevNav';
+import { useState, useEffect } from "react";
+import "./App.css";
+import DevCard from "./components/DevCard";
+import DevNavBar from "./components/DevNav";
+import Nav from "./components/Nav";
 
 function App() {
-  useEffect (
-    () => {
-      alert ("Hello Wilder ! Mets tes lunettes on va t'éblouir 😎 (ou pas)") 
-    },
-    []
-  );
-  const [devIndex, setDevIndex] = useState(0)
-  const devList = [
-    { name: "Aurelien",
-      imgSrc: "./images-indiv/_MG_8012.JPG",
-    },
-     { name: "Aghiles",
-      imgSrc: "./images-indiv/_MG_8004.JPG"
-    },
-     { name: "Pierre",
-      imgSrc: "public/images-indiv/_MG_8017.JPG",
-    },
-     { name: "Raph le B",
-      imgSrc: "./images-indiv/_MG_8008.JPG",
-    },
-    ]
-    return (
-    <>
-    <div>
-      <DevCard
-        name= {devList[devIndex].name}
-        imgSrc= {devList[devIndex].imgSrc} />
-    </div>
-      
-      <DevNavBar 
-        devIndex={devIndex} 
-        setDevIndex={setDevIndex} 
-        devList={devList}/>
-        </>
-  );
+	useEffect(() => {
+		alert("Hello Wilder ! Mets tes lunettes on va t'éblouir 😎 (ou pas)");
+	}, []);
+	const [devIndex, setDevIndex] = useState(0);
+	const devList = [
+		{ name: "Aurelien", imgSrc: "./images-indiv/_MG_8012.JPG" },
+		{ name: "Aghiles", imgSrc: "./images-indiv/_MG_8004.JPG" },
+		{ name: "Pierre", imgSrc: "public/images-indiv/_MG_8017.JPG" },
+		{ name: "Raph le B", imgSrc: "./images-indiv/_MG_8008.JPG" },
+	];
+	return (
+		<>
+			<Nav />
+			<h1 className="title">Team Dev Wild Code School 2024</h1>
+			<div>
+				<DevCard
+					name={devList[devIndex].name}
+					imgSrc={devList[devIndex].imgSrc}
+				/>
+			</div>
+
+			<DevNavBar
+				devIndex={devIndex}
+				setDevIndex={setDevIndex}
+				devList={devList}
+			/>
+		</>
+	);
 }
-export default App
+export default App;
---
+++ teamrush/src/components/Nav.tsx
@@ -0,0 +1,17 @@
+export default function Nav() {
+	return (
+		<nav>
+			<ul className="navigation">
+				<li>
+					<a href="https://www.google.fr/">Home</a>
+				</li>
+				<li>
+					<a href="https://www.google.fr/">About</a>
+				</li>
+				<li>
+					<a href="https://www.google.fr/">contact Us</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
